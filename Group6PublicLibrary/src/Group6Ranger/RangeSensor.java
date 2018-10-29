e033f93e55c41d09b8779f335a35bc20e353535b
Paul Borysewicz <paul.borysewicz@gmail.com>
2018-10-29 13:21:51 -0400

    A well-behaved methods to RangeSensor

--- Group6PublicLibrary/src/Group6Ranger/RangeSensor.java
+++ Group6PublicLibrary/src/Group6Ranger/RangeSensor.java
@@ -9,15 +9,20 @@ import org.iot.raspberry.grovepi.devices.GroveUltrasonicRanger;
  */
 public abstract class RangeSensor{
     private List<IRangeObserver> observers = new ArrayList<IRangeObserver>();
-     GroveUltrasonicRanger ranger;
+    GroveUltrasonicRanger ranger;
 
-     protected boolean running = false;
+    protected boolean running = false;
+    protected GrovePi grovePi;
+    protected int pin;
+
      /**
      * Constructor - initializes an instance of the GroveUltrasonicRanger
      * @param grovePi Reference to the GrovePi object
      * @param pin The pin the Sensor is attached to on the Pi itself
      */
     public RangeSensor(GrovePi grovePi, int pin) {
+        this.grovePi = grovePi;
+        this.pin = pin;
         ranger = new GroveUltrasonicRanger(grovePi, pin);
     }
     
@@ -51,5 +56,40 @@ public abstract class RangeSensor{
         observers.forEach((o) -> {
             o.update(this, range);
         });
+    }
+    
+    /*
+     * Checks 'this' and 'o' for equality
+     * @param o object to check against 'this' for equality
+     */
+    @Override 
+    public boolean equals(Object o) {
+        if (o == null) {
+            return false;
+        }
+        
+        if (o == this) {
+            return true;
+        }
+     
+        if (o.getClass() != this.getClass()) {
+            return false;
+        }
+        
+        RangeSensor that = (RangeSensor) o;
+        return Objects.equals(this.grovePi, that.grovePi) && this.pin == that.pin;
+    }
+    
+    @Override 
+    public int hashCode() {
+        int hashCode = 0;
+        hashCode += grovePi.hashCode();
+        hashCode += pin;
+        return hashCode;
+    }
+    
+    @Override 
+    public String toString() {
+        return String.format("%s %s %s", this.getClass().getSimpleName(), grovePi, pin);
     }
 }
